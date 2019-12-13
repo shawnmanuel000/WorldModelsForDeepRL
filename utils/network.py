0230a0e048ce6b0d1a2c9c39100cc5c8d35dd728
Shawn <shawnmanuel000@gmail.com>
2019-12-13 01:14:44 -0800

    Checkpoint

--- utils/network.py
+++ utils/network.py
@@ -142,6 +142,80 @@ class PTACAgent(RandomAgent):
 	def train(self, state, action, next_state, reward, done):
 		pass
 
+class NoisyLinear(torch.nn.Linear):
+	def __init__(self, in_features, out_features, sigma_init=0.017, bias=True):
+		super().__init__(in_features, out_features, bias=bias)
+		self.sigma_weight = torch.nn.Parameter(torch.Tensor(out_features, in_features).fill_(sigma_init))
+		self.register_buffer("epsilon_weight", torch.zeros(out_features, in_features))
+		if bias:
+			self.sigma_bias = torch.nn.Parameter(torch.Tensor(out_features).fill_(sigma_init))
+			self.register_buffer("epsilon_bias", torch.zeros(out_features))
+		self.reset_parameters()
+
+	def reset_parameters(self):
+		std = math.sqrt(3 / self.in_features)
+		torch.nn.init.uniform_(self.weight, -std, std)
+		torch.nn.init.uniform_(self.bias, -std, std)
+
+	def forward(self, input):
+		torch.randn(self.epsilon_weight.size(), out=self.epsilon_weight)
+		bias = self.bias
+		if bias is not None:
+			torch.randn(self.epsilon_bias.size(), out=self.epsilon_bias)
+			bias = bias + self.sigma_bias * torch.autograd.Variable(self.epsilon_bias)
+		weight = self.weight + self.sigma_weight * torch.autograd.Variable(self.epsilon_weight)
+		return torch.nn.functional.linear(input, weight, bias)
+
+# class NoisyLinear(nn.Module):
+#     def __init__(self, in_features, out_features, std_init=0.4):
+#         super(NoisyLinear, self).__init__()
+        
+#         self.in_features  = in_features
+#         self.out_features = out_features
+#         self.std_init     = std_init
+        
+#         self.weight_mu    = nn.Parameter(torch.FloatTensor(out_features, in_features))
+#         self.weight_sigma = nn.Parameter(torch.FloatTensor(out_features, in_features))
+#         self.register_buffer('weight_epsilon', torch.FloatTensor(out_features, in_features))
+        
+#         self.bias_mu    = nn.Parameter(torch.FloatTensor(out_features))
+#         self.bias_sigma = nn.Parameter(torch.FloatTensor(out_features))
+#         self.register_buffer('bias_epsilon', torch.FloatTensor(out_features))
+        
+#         self.reset_parameters()
+#         self.reset_noise()
+    
+#     def forward(self, x):
+#         if self.training: 
+#             weight = self.weight_mu + self.weight_sigma.mul(Variable(self.weight_epsilon))
+#             bias   = self.bias_mu   + self.bias_sigma.mul(Variable(self.bias_epsilon))
+#         else:
+#             weight = self.weight_mu
+#             bias   = self.bias_mu
+        
+#         return F.linear(x, weight, bias)
+    
+#     def reset_parameters(self):
+#         mu_range = 1 / math.sqrt(self.weight_mu.size(1))
+        
+#         self.weight_mu.data.uniform_(-mu_range, mu_range)
+#         self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.weight_sigma.size(1)))
+        
+#         self.bias_mu.data.uniform_(-mu_range, mu_range)
+#         self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.bias_sigma.size(0)))
+    
+#     def reset_noise(self):
+#         epsilon_in  = self._scale_noise(self.in_features)
+#         epsilon_out = self._scale_noise(self.out_features)
+        
+#         self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+#         self.bias_epsilon.copy_(self._scale_noise(self.out_features))
+    
+#     def _scale_noise(self, size):
+#         x = torch.randn(size)
+#         x = x.sign().mul(x.abs().sqrt())
+#         return x
+
 def init_weights(m):
 	if type(m) == torch.nn.Linear:
 		torch.nn.init.normal_(m.weight, mean=0., std=0.1)
